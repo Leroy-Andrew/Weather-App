f2d989806eaeca7cc0f88263aaaa56050f393996
Leroy Oliver <leroyo@hotmail.com>
2023-05-13 22:49:33 +0100

    finish app.js

---
+++ app.js
@@ -0,0 +1,53 @@
+const apikey = "27c401f6e81e2277b2cc3fcf3e6aed1f";
+
+const weatherData = document.getElementById("weather-data");
+const cityInput = document.getElementById("city-input");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const cityValue = cityInput.value;
+    getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`)
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok")
+        }
+
+        const data = await response.json()
+
+        const temperature = Math.round(data.main.temp)
+        const description = data.weather[0].description
+        const icon = data.weather[0].icon
+        const details = [
+            `Feels like: ${Math.round(data.main.feels_like)}`,
+            `Humidity: ${data.main.humidity}%`,
+            `Wind Speed: ${data.wind.speed} m/s`,
+        ];
+        weatherData.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="weather icon">`;
+        weatherData.querySelector(
+            ".temperature").textContent = `${temperature}°C`;
+
+        weatherData.querySelector(
+           ".description").textContent = `${description}`;
+
+        weatherData.querySelector(
+           ".details").innerHTML = details.map((detail) => `<div>${detail}</div>`).join("");
+
+        
+    } catch (error) {
+        weatherData.querySelector(".icon").innerHTML = "";
+        weatherData.querySelector(
+            ".temperature").textContent = "";
+
+        weatherData.querySelector(
+           ".description").textContent = "Error: Please try again";
+
+        weatherData.querySelector(
+           ".details").innerHTML = "";
+    }
+}
