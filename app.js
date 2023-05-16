2009c98881919f2177fd28e4a1000c486852b3d7
Leroy Oliver <leroyo@hotmail.com>
2023-05-16 08:25:36 +0100

    modify files

--- app.js
+++ app.js
@@ -34,11 +34,11 @@ async function getWeatherData(cityValue) {
 
         weatherData.querySelector(
            ".description").textContent = `${description}`;
-           if(description == "scattered clouds") {
+           if(description == "few clouds") {
             document.body.style.backgroundImage ="url('assets/cloudy.jpg')";
         } else if (description == "clear sky") {
             document.body.style.backgroundImage ="url('assets/sunny.jpg')";
-        } else if (description == "light rain", "heavy intensity rain") {
+        } else if (description == "light rain") {
             document.body.style.backgroundImage ="url('assets/rain.jpg')";
         }
 
